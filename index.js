6172b507ce13b4da940aefefd7c197902bb62a49
Fran Blanco <arbust908@gmail.com>
2021-09-25 11:25:02 -0300

    Improve::ClearAnyConsole

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
+const clearConsole = require('clear-any-console');
+clearConsole();
 console.log(`
     Hola soy Pancho!
     Re capo, no?
     https://panchoblanco.com
-    
 `);
