0bfd4a519973d8fdf1dd4459a9143efe6bf157cb
Fran Blanco <arbust908@gmail.com>
2021-09-25 11:43:57 -0300

    Feat::New Welcome

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console');
-clearConsole();
+const welcome = require('cli-welcome');
+const pkgJSON = require('./package.json');
+
+welcome({
+    title: pkgJSON.name,
+    tagLine: 'npx panchoblanco',
+    description: pkgJSON.description,
+    version: pkgJSON.version,
+    bgColor: '#F43F5E',
+    color: '#ffffff',
+    bold: true,
+    clear: true,
+});
+
 console.log(`
     Hola soy Pancho!
     Re capo, no?
